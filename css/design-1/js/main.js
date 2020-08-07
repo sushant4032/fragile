aa21a7500be2062e39aafa6aef536c8bd805df05
sushant4032 <sushant4032@gmail.com>
2020-08-07 09:46:43 +0530

    styled scrollbar for cilsite

---
+++ css/design-1/js/main.js
@@ -0,0 +1,9 @@
+document.querySelector('.sidebar-opener').addEventListener('click', () => {
+    document.querySelector('.sidebar').style.display = 'block';
+});;
+
+document.querySelector('.sidebar-closer').addEventListener('click', () => {
+    document.querySelector('.sidebar').style.display = 'none';
+});
+
+
